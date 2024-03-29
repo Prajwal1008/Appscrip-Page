5522d00cdd6743b4b97879fda5b1f1544f5aa311
Prajwal1008 <prajwalbankar117@gmail.com>
2024-03-30 00:08:42 +0530

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
-import "@/styles/globals.css";
+import '../styles/Home.module.css'
 
 export default function App({ Component, pageProps }) {
+  
+  <script src="https://kit.fontawesome.com/551ef3a7d4.js" crossOrigin="anonymous"></script>
   return <Component {...pageProps} />;
+
 }
